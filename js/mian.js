744b47111e3e86745c7aab511149ac02ea95f363
Egor Kirin <egorcom27@gmail.com>
2022-06-18 02:17:23 +0300

    price block

--- js/mian.js
+++ js/mian.js
@@ -1,3 +1,5 @@
+// Slider
+
 $(function () {
     $('.bike-slider, .slider__items').slick({
         arrows: false,
@@ -7,3 +9,27 @@ $(function () {
         autoplaySpeed: 2000
     });
 })
+
+
+// Price
+
+const priceBtns = document.querySelectorAll('.form__btn-box button');
+const priceItems = document.querySelectorAll('.price__item');
+
+function hidePrice() {
+    priceItems.forEach(item => item.classList.add('hide'));
+    priceBtns.forEach(btn => btn.classList.remove('active'));
+}
+
+function showPrice(index) {
+    priceItems[index].classList.remove('hide');
+    priceBtns[index].classList.add('active');
+}
+
+hidePrice();
+showPrice(0);
+
+priceBtns.forEach((btn, index) => btn.addEventListener('click', () => {
+    hidePrice();
+    showPrice(index)
+}));
